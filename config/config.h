f6fdfac4189ae63d55309f405775bb6aebc7e61a
GatoAmarilloBicolor <cosmogatokat@gmail.com>
2026-01-17 18:38:31 -0500

    Add missing config.h to config/ directory for build

---
+++ config/config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Build configuration for AMDGPU_Abstracted
+
+// OS detection
+#if defined(__HAIKU__)
+#define OS_HAIKU 1
+#define OS_LINUX 0
+#define OS_FREEBSD 0
+#elif defined(__linux__)
+#define OS_HAIKU 0
+#define OS_LINUX 1
+#define OS_FREEBSD 0
+#elif defined(__FreeBSD__)
+#define OS_HAIKU 0
+#define OS_LINUX 0
+#define OS_FREEBSD 1
+#else
+#error "Unsupported OS"
+#endif
+
+// Userland mode
+#ifndef USERLAND_MODE
+#define USERLAND_MODE 0
+#endif
+
+// Debug settings
+#define AMD_DEBUG 0
+#define AMD_LOG_LEVEL 1
+
+#endif // CONFIG_H
